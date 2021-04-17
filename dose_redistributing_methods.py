8f8aef45df1424a82fc0441df234406ae5776c6a
Abraham Hinteregger <oerpli@hotmail.com>
2021-04-17 11:39:57 +0200

    added fractional doses

--- dose_redistributing_methods.py
+++ dose_redistributing_methods.py
@@ -10,7 +10,9 @@ from data_loading import D0, D1, D2
 # to give a vaccination to someone that is from a group that benefits most from it
 # 95 - 89 = 6, i.e. 2nd dose is roughly 15x less effective
 # if X has a 15x higher risk of dying from COVID than Y, it's better to give X a second dose vs Y a first dose.
-def redistribute_doses(df, pop, *, distr_first=False, priority=None) -> pd.DataFrame:
+def redistribute_doses(
+    df, pop, *, distr_first=False, priority=None, fractional_dosing=False
+) -> pd.DataFrame:
     """Distribute doses according to priority, s.t. lives saved is maximized.
     Total number of vaccinations for each day does not change (as it is assumed that this is limited by capacity/supply).
 
@@ -50,16 +52,33 @@ def redistribute_doses(df, pop, *, distr_first=False, priority=None) -> pd.DataF
         (D1, "85-99"),
     ]
 
+    fractional_doses = {
+        "25-34": 0.25,
+        "35-44": 0.25,
+        "45-54": 0.25,
+        "55-64": 0.5,
+        "65-74": 0.75,
+        "75-84": 1,
+        "85-99": 1,
+    }
+
     def get_remainder_d(d, group, vacc):
         # how many stay in group, how many can go to other groups (according to priority)
         unvacc = pop[group] - have_d[(d, group)]
+
         missing = min(vacc, unvacc)  # this many still need their d-th shot
-        remainder = vacc - missing  # this is overflow, give to highest priority group
+        # this is overflow, give to (next) highest priority group
+        remainder = vacc - missing
+        if fractional_dosing:
+            overflow = missing * (1 - fractional_doses[group])
+            overflow = int(overflow)
+            remainder += overflow
         return missing, remainder
 
     def assign_d(d, group, vacc, row):
         # update data structures with vacc new d-th dose vaccines for age group
         have_d[(d, group)] += vacc  # dose/group lvl vacc counter
+
         if have_d[(d, group)] == pop[group]:
             # print(f"Finished {d} for {grp}")
             priorities.remove((d, group))
@@ -81,3 +100,6 @@ def redistribute_doses(df, pop, *, distr_first=False, priority=None) -> pd.DataF
         # finished updating row, save to list
         rows.append(row_new)
     return pd.DataFrame(rows, index=df.index)
+
+
+# %%
